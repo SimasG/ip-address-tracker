e135dae82cc275744c56f2a7e311b9fac6354d44
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-19 22:47:40 +0200

    initial styling for mobile done

--- script.js
+++ script.js
@@ -4,16 +4,22 @@
 // const apiData = {
 //   url: "https://api.ipgeolocation.io/",
 // };
+const url =
+  "https://api.ipgeolocation.io/ipgeo?apiKey=8a1a101fc03c4006a08b3109d1ba1c63";
 
-async function getData() {
-  const response = await fetch(
-    `https://api.ipgeolocation.io/ipgeo?apiKey=8a1a101fc03c4006a08b3109d1ba1c63`
-  );
-  const data = await response.json();
-  console.log(data.ip);
+// async function getData() {
+//   const response = await fetch(
+//     `https://api.ipgeolocation.io/ipgeo?apiKey=8a1a101fc03c4006a08b3109d1ba1c63`
+//   );
+//   const data = await response.json();
+//   console.log(data.ip);
 
-  const testHeading = document.querySelector(".test-heading");
-  testHeading.innerHTML = data.ip;
-}
+//   const testHeading = document.querySelector(".test-heading");
+//   testHeading.innerHTML = data.ip;
+// }
 
-getData();
+// getData();
+
+fetch(url)
+  .then((data) => data.json())
+  .then((ipData) => console.log(ipData));
