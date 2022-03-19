0c4a3664f96d94759e1e095de6ecc39626c3f87d
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-20 00:00:24 +0200

    connected the API data points to the front end

--- README-template.md
+++ README-template.md
@@ -10,3 +10,16 @@ ipgeolocation.io
 https://leafletjs.com/SlavaUkraini/
 
 - Spacial map representation
+
+// async function getData() {
+// const response = await fetch(
+// `https://api.ipgeolocation.io/ipgeo?apiKey=8a1a101fc03c4006a08b3109d1ba1c63`
+// );
+// const data = await response.json();
+// console.log(data.ip);
+
+// const testHeading = document.querySelector(".test-heading");
+// testHeading.innerHTML = data.ip;
+// }
+
+// getData();
--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-// https://api.ipgeolocation.io
-// 8a1a101fc03c4006a08b3109d1ba1c63
-
-// const apiData = {
-//   url: "https://api.ipgeolocation.io/",
-// };
 const url =
   "https://api.ipgeolocation.io/ipgeo?apiKey=8a1a101fc03c4006a08b3109d1ba1c63";
 
-// async function getData() {
-//   const response = await fetch(
-//     `https://api.ipgeolocation.io/ipgeo?apiKey=8a1a101fc03c4006a08b3109d1ba1c63`
-//   );
-//   const data = await response.json();
-//   console.log(data.ip);
+const ipAddress = document.querySelector(".ip-address");
+const ipLocation = document.querySelector(".ip-location");
+const timezoneOffset = document.querySelector(".timezone-offset");
+const isp = document.querySelector(".isp");
 
-//   const testHeading = document.querySelector(".test-heading");
-//   testHeading.innerHTML = data.ip;
-// }
-
-// getData();
+window.addEventListener("load", fetch(url));
 
 fetch(url)
   .then((data) => data.json())
-  .then((ipData) => console.log(ipData));
+  .then((ipData) => populateData(ipData));
+
+const populateData = (fetchedData) => {
+  console.log(fetchedData);
+  const utcDate = new Date().toString().match(/([A-Z]+[\+-][0-9]+)/)[1];
+
+  ipAddress.innerHTML = fetchedData.ip;
+  ipLocation.innerHTML = `${fetchedData.city}, ${fetchedData.state_prov} ${fetchedData.zipcode}`;
+  timezoneOffset.innerHTML = utcDate;
+  isp.innerHTML = fetchedData.isp;
+};
+
+// const offset = new Date().getTimezoneOffset();
+// console.log(offset);
+
+// const timezony = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// console.log(timezony);
