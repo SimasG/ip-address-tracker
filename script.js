0bb1b50acda1fd3f980cf90f4021c74d70c0efad
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-20 18:53:33 +0200

    progressed with domain name resolution to ip addresses

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // TODO: Ensure the base case works (every user sees info about their IP Address by default) -> DONE
-// TODO: Add ability to input other IP addresses & get the remaining info
+// TODO: Add ability to input other IP addresses & get the remaining info -> DONE
 // TODO Add ability to input domain names & get the info
+// TODO: Handle cases where there is no info
 // TODO: Maps functionality -> make LeafletJS work
+// TODO: (maybe) sharpen up UI (info messages, disclaimers, etc.)
 
-const url =
-  "https://api.ipgeolocation.io/ipgeo?apiKey=112af9d778a540e0bf4baba8316ab539";
+// TODO: (later) hide the API key
+// require("dotenv").config();
+// console.log(process.env);
+// const apiKey = process.env.API_KEY;
+const apiKey = "112af9d778a540e0bf4baba8316ab539";
 
 const ipAddress = document.querySelector(".ip-address");
 const ipLocation = document.querySelector(".ip-location");
@@ -13,16 +18,17 @@ const isp = document.querySelector(".isp");
 const form = document.querySelector(".search-container");
 const input = document.querySelector("#ip-address-input");
 
+const url = "https://api.ipgeolocation.io/ipgeo?apiKey=";
 let address;
 
 async function getIpAddress(address) {
   if (address) {
-    const response = await fetch(`${url}/&ip=${address}`);
+    const response = await fetch(`${url}${apiKey}&ip=${address}`);
     const data = await response.json();
     console.log(data);
     displayData(data);
   } else {
-    const response = await fetch(url);
+    const response = await fetch(`${url}${apiKey}`);
     const data = await response.json();
     console.log(data);
     displayData(data);
